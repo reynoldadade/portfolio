27312afc8162894c4399a3b50529ea5155a8c9ea
Reyold Osei Adade <reynoldadade@gmail.com>
2020-03-11 15:54:46 +0000

    started material

---
+++ src/hoc/layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+const layout = props => {
+  return (
+    <div>
+      <Navbar />
+      <div>Profile</div>
+      <div>Projects, skills,</div>
+      <div>blog posts</div>
+    </div>
+  );
+};
+
+export default layout;
