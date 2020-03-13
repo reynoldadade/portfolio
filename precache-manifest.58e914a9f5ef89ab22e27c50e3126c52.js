ac44678421a9a54b8ce6567bb2c8d68d8b7221dc
Reyold Osei Adade <reynoldadade@gmail.com>
2020-03-13 16:43:40 +0000

    Updates

---
+++ precache-manifest.58e914a9f5ef89ab22e27c50e3126c52.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b4f8a1b23bbe7f3595282af4a39563",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "25ad616c978485102c4a",
+    "url": "/portfolio/static/css/main.282f268d.chunk.css"
+  },
+  {
+    "revision": "3a3527f95c0061b22699",
+    "url": "/portfolio/static/js/2.cf5a5fd9.chunk.js"
+  },
+  {
+    "revision": "4214db9a6c23cd334176e62fe82579f5",
+    "url": "/portfolio/static/js/2.cf5a5fd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25ad616c978485102c4a",
+    "url": "/portfolio/static/js/main.f90c65b7.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "66650f4fc71e964752271fa81ae0af1f",
+    "url": "/portfolio/static/media/profile.66650f4f.png"
+  },
+  {
+    "revision": "860022d69ff64b453d6ad659b62e5937",
+    "url": "/portfolio/static/media/project-1.860022d6.png"
+  },
+  {
+    "revision": "70cb7da61541bf05c3f92cebe3b05649",
+    "url": "/portfolio/static/media/project-2.70cb7da6.png"
+  },
+  {
+    "revision": "6967551ab4f9f62cd8e05c45e2ce2318",
+    "url": "/portfolio/static/media/project-3.6967551a.png"
+  },
+  {
+    "revision": "880494f98ca4cae43af540508b66f507",
+    "url": "/portfolio/static/media/project-4.880494f9.png"
+  },
+  {
+    "revision": "0218091d873565697449bf724d4cd9a7",
+    "url": "/portfolio/static/media/project-5.0218091d.png"
+  }
+]);
